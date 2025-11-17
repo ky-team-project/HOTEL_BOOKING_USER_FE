24ad2e9b2d472f4b9277eaf1af83cc0c626a8f16
joonsol <71648638+joonsol@users.noreply.github.com>
2025-11-17 09:10:17 +0900

    Header Footer setting

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
 
 const MainLayout = () => {
   return (
     <div className="main-layout">
-      <header>{/* 헤더 네비게이션 */}</header>
+      <Header />
 
       <main>
         <Outlet />
       </main>
 
-      <footer>{/* 푸터 */}</footer>
+      <Footer />
     </div>
   );
 };
